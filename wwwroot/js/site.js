aa3e6a5158d91a7e5aacca91f05c5d2d841ecea2
RAMIRO SUED <47958668@ort>
2023-10-12 15:02:50 -0300

    v4

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -9,6 +9,10 @@ function verificarContraseña() {
     let campoContraseña = document.getElementById('contraseña1').value;
     let campoContraseña2 = document.getElementById('contraseña2').value;
     let verificada=campoContraseña.length >= 8 && campoContraseña==campoContraseña2
+    if (verificada==false){
+        const error=document.getElementById('errorIngreso')
+        error.innerHTML="Revise que las contraseñas sean iguales o que tengan 8 dígitos o más"
+    }
     return verificada
 
 }
